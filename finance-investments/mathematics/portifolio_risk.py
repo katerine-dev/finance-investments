2739ecce4bdcfb410fe0920b7fa41f9e96bfbb30
katewitkoski <ka.lwitkoski@gmail.com>
2023-11-27 15:50:25 -0300

    Diversifiable Risk

--- finance-investments/mathematics/portifolio_risk.py
+++ finance-investments/mathematics/portifolio_risk.py
@@ -32,3 +32,45 @@ print(pfolio_var)
 pfolio_vol = (np.dot (weights.T, np.dot(sec_returns.cov() * 250, weights))) **0.5
 print(str(round(pfolio_vol, 5) * 100) + '%')
 
+# Calculating Diversifiable and Non-Diversifiable Risk of a Portfolio
+
+print(weights[0])
+print(weights[1])
+
+# diversifiable risk = portfolio variance - weighted annual variance
+# Diversifiable Risk
+
+PG_var_a = sec_returns[['PG']].var() * 250
+print(PG_var_a)
+
+BEI_var_a = sec_returns[['BEI.DE']].var() * 250
+print(BEI_var_a)
+
+dr = pfolio_var - (weights[0] ** 2 * PG_var_a) - (weights[1] ** 2 * BEI_var_a)
+
+print(dr)
+
+#print(float(PG_var_a))
+
+# Correct
+
+PG_var_a = sec_returns['PG'].var() * 250
+print(PG_var_a)
+
+BEI_var_a = sec_returns['BEI.DE'].var() * 250
+print(BEI_var_a)
+
+dr = pfolio_var - (weights[0] ** 2 * PG_var_a) - (weights[1] ** 2 * BEI_var_a)
+
+print(dr)
+
+print(str(round(dr*100, 3)) + '%')
+
+# Non-diversifiable risk
+n_dr_1 = pfolio_var - dr
+print(n_dr_1)
+
+n_dr_2 = (weights[0] ** 2 * PG_var_a) - (weights[1] ** 2 * BEI_var_a)
+print(n_dr_2)
+
+n_dr_2 == n_dr_1
